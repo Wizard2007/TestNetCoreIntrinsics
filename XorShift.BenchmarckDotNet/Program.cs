006c190401d12ac956b2eda63e82f124bc87b2d7
Eujine <sponsor85@mail.ru>
2019-12-17 18:04:04 +0200

    [*] code clean up

---
+++ XorShift.BenchmarckDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XorShift.BenchmarckDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- XorShift.Intrinsics/XorshiftUnrolled64Ex.cs
+++ XorShift.Intrinsics/XorshiftUnrolled64Ex.cs
@@ -9,36 +9,38 @@ namespace XorShift.Intrinsics
 
         protected unsafe override void FillBuffer(byte[] buf, int offset, int offsetEnd)
         {
-            var vectorArray = stackalloc ulong[4];
-            var tVectorArray = stackalloc ulong[4];
-            
-            ulong* pX = vectorArray;
-            ulong* pY = vectorArray+1;
-            ulong* pZ = vectorArray+2;
-            ulong* pW = vectorArray+3;
+            var vectorArray = stackalloc uint[4];
+            var tVectorArray = stackalloc uint[4];
+
+            uint* pX = vectorArray;
+            uint* pY = vectorArray+1;
+            uint* pZ = vectorArray+2;
+            uint* pW = vectorArray+3;
 
             *(pX) = _x;
             *(pY) = _y;
             *(pZ) = _z;
             *(pW) = _w;
 
-            ulong* pTX = tVectorArray;
-            ulong* pTY = tVectorArray+1;
-            ulong* pTZ = tVectorArray+2;
-            ulong* pTW = tVectorArray+3;
+            uint* pTX = tVectorArray;
+            uint* pTY = tVectorArray+1;
+            uint* pTZ = tVectorArray+2;
+            uint* pTW = tVectorArray+3;
 
             fixed (byte* pbytes = buf)
             {
-                ulong* pbuf = (ulong*) (pbytes + offset);
-                ulong* pend = (ulong*) (pbytes + offsetEnd);
+                var pbuf = (uint*) (pbytes + offset);
+                var pend = (uint*) (pbytes + offsetEnd);
+
                 while (pbuf < pend)
                 {
                     var  v = Avx2.LoadVector256(vectorArray);
-                    var shifted = Avx2.ShiftLeftLogical(v, 11);
-                    var tVector = Avx2.Xor(v, shifted);
-                    var tVectorR = Avx2.ShiftRightLogical(tVector, 8);
-                    var tResult = Avx2.Xor(tVector, tVectorR);
-                    Avx2.Store(tVectorArray, tResult);
+
+                    var tXYXW = Avx2.Xor(v, Avx2.ShiftLeftLogical(v, 11));
+
+                    var tXYZWShifted = Avx2.ShiftRightLogical(tXYXW, 8);
+
+                    Avx2.Store(tVectorArray, Avx2.Xor(tXYXW, tXYZWShifted));
 
                     *(pbuf++) = *(pX) = *(pW) ^ (*(pW) >> 19) ^ *(pTX);
                     *(pbuf++) = *(pY) = *(pX) ^ (*(pX) >> 19) ^ *(pTY);
